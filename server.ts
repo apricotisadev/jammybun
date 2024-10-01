2b9980e8bebed2dca143fb533269d39bf4d00065
rockalbert466@gmail.com <rockalbert466@gmail.com>
2024-10-01 11:41:21 +0530

    Express Wrapper

--- server.ts
+++ server.ts
@@ -1,7 +1,21 @@
 import { Bot } from "./src/index";
 import dotenv from "dotenv"
+import express from "express"
 
 dotenv.config();
 
-const bot = new Bot();
-bot.start(process.env.DISCORD_TOKEN);
+const app = express();
+const port = 16969;
+
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.send("<html><body><h1>A Discord Bot</h1></body></html>");
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+    const bot = new Bot();
+    bot.start(process.env.DISCORD_TOKEN);
+})
+
